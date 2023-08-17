a307a7e7320e780660b98463a7a252852f55b568
Daniel Carvalho <danielcfleite@gmail.com>
2023-08-17 10:37:38 -0300

    new

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const startButton = document.querySelector("#start");
 const pg2 = document.querySelector(".pg2");
 
 startButton.addEventListener("click", () => {
-  pg2.style.animation = "page 2s 2s forwards";
+  pg2.style.animation = "page 2s forwards";
   console.log(pg2);
 });
