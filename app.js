61221c5a64aec05546b869e5496daefcb67986c5
Daniel Carvalho <danielcfleite@gmail.com>
2023-08-17 09:34:56 -0300

    trying again

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const startButton = document.querySelector("#start");
+const pg2 = document.querySelector(".pg2");
 
-startButton.addEventListener("click", () => {});
+startButton.addEventListener("click", () => {
+  pg2.style.animation = "page 2s 2s forwards";
+  console.log(pg2);
+});
