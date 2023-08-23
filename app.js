baffbe7069a43bbb3033608503cd31d6d6ad9711
Daniel Carvalho <danielcfleite@gmail.com>
2023-08-23 14:53:27 -0300

    prototype mobile design

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const startButton = document.querySelector("#start");
 const pg2 = document.querySelector(".pg2");
+const pg1 = document.querySelector(".pg2");
+const header = document.querySelector(".header");
 
 startButton.addEventListener("click", () => {
-  pg2.style.animation = "page 2s forwards";
+  pg2.style.top = "0";
+  header.classList.remove("hidden");
   console.log(pg2);
 });
